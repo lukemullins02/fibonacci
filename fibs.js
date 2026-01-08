f5b9ba5756b4455056e470c21618b47a981ffacd
= <lukejmullins@gmail.com>
2026-01-08 11:12:59 -0500

    Finish fibonacci

---
+++ fibs.js
@@ -0,0 +1,31 @@
+function fibs(num) {
+  let arr = [];
+  let prev1;
+  let prev2;
+
+  for (let i = 0; i <= num; i++) {
+    if (i < 2) arr.push(i);
+    if (i >= 2) {
+      prev1 = arr[i - 2];
+      prev2 = arr[i - 1];
+      arr.push(prev1 + prev2);
+    }
+  }
+
+  return arr;
+}
+
+let arr = [];
+
+function fibsRec(n) {
+  if (n < 2) {
+    return [0, 1];
+  } else {
+    const arr = fibsRec(n - 1);
+    arr.push(arr[n - 1] + arr[n - 2]);
+    return arr;
+  }
+}
+
+console.log(fibsRec(8));
+console.log(fibs(8));
